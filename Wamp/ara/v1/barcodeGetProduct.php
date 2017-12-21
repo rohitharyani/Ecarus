c27bcf0b832a51556e3c6c2ce4810dee67be3829
Rohit Haryani <rohit.haryani196@gmail.com>
2017-12-20 16:29:42 -0800

    Create barcodeGetProduct.php

---
+++ Wamp/ara/v1/barcodeGetProduct.php
@@ -0,0 +1,21 @@
+<?php
+
+	require_once '../includes/DbOperations.php';
+	
+	$response = array();
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+
+		if(isset($_POST['barcode'])){
+
+			$db = new DbOperation();
+
+
+			$result = $db->barcodeGetProduct($_POST['barcode']);
+		}
+		else{
+			$response['error'] = true;
+			$response['message'] = "Invalid request";
+			echo json_encode($response);
+		}
+	}
+?>	
