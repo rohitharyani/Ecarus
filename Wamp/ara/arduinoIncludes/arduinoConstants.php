375be9279259c470651d46c018560728a1d37441
Rohit Haryani <rohit.haryani196@gmail.com>
2017-12-20 16:41:20 -0800

    Create arduinoConstants.php

---
+++ Wamp/ara/arduinoIncludes/arduinoConstants.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_NAME','ara');  //Database name
+	define('DB_USER','root'); //User name
+	define('DB_PASSWORD',''); //Password
+	define('DB_HOST','localhost'); //Ip address of the server
+?>
